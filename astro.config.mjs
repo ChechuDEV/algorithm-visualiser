102da191d93d5f4d8c778da12ba4bc4d711a05bb
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2023-01-10 18:52:21 +0100

    ✨ Added Astro/Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://algorithms.chechu.dev/",
+  integrations: [
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date("2023-01-10"),
+    }),
+  ],
+});
